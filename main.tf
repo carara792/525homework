a90c1b680f933e390955a1ddb4b8dfe15f557c04
Cara <carara.792@gmail.com>
2021-05-26 00:08:49 +0800

    3

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ source = "terraform-aws-modules/vpc/aws"
 Name = "UT-Env"
 }
 
-module "s3-bucket" {
-source = "terraform-aws-modules/s3-bucket/aws"
-}
+#module "s3-bucket" {
+#source = "terraform-aws-modules/s3-bucket/aws"
+#}
 
 module "ec2-instance" {
 source = "terraform-aws-modules/ec2-instance/aws"
